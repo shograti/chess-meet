c04d095eadc23ec6c3dbf898176d60936730a53e
shograti <petitberghienleo@gmail.com>
2024-05-24 01:36:53 +0200

    Added basic leaflet map

---
+++ client/src/components/map.tsx
@@ -0,0 +1,22 @@
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
+export const Map = () => {
+  return (
+    <MapContainer
+      className="h-[600px] rounded-sm mt-6"
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={true}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,10 +1,23 @@
+import { Map } from "@/components/map";
 import MaxWidthWrapper from "@/components/max-width-wrapper";
+import { ChevronDown, ChevronUp } from "lucide-react";
+import { useState } from "react";
 
 export const Home = () => {
-
+  const [isMapVisible, setIsMapVisible] = useState(true);
   return (
     <MaxWidthWrapper>
-      <div className="mt-6">Home</div>
+      <div className="mt-6"></div>
+      <h2 className="flex gap-2 items-center text-xl">
+        <span>Map</span>
+        <button
+          onClick={() => setIsMapVisible((prev) => !prev)}
+          className="flex items-center gap-1"
+        >
+          {isMapVisible ? <ChevronUp /> : <ChevronDown />}
+        </button>
+      </h2>
+      {isMapVisible && <Map />}
     </MaxWidthWrapper>
   );
 };
